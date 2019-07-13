67a5af7c0ffe346d8eb112b260bec13729c55917
Andre Garcia <Andre.Garcia@la.logicalis.com>
2019-07-12 21:48:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import ModalExample from './Modal'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  a = 0
+  ola = (num) => {
+    this.a += num
+    console.log(this.a)
+  }
+  render(){
+    console.log(this.a)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <ModalExample b={this.ola}/>
+          <Button color="primary">primary</Button>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
